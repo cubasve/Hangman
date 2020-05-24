12c30452906f4e8fb590c28e4ae30046b8777c02
cubasve <62129720+cubasve@users.noreply.github.com>
2020-05-24 10:46:47 -0400

    Upload js/main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*-------CONSTANTS--------*/ 
+const animalArray = ['hamster', '']
+
+
+/*-------APP'S STATE (VARIABLES)--------*/ 
+
+
+/*-------CACHED ELEMENT REFERENCES--------*/ 
+
+
+/*-------EVENT LISTENERS--------*/ 
+
+
+/*-------FUNCTIONS--------*/ 
