61ed27164510de509698fa42b622209e65a5e65a
Eva Cubas Vasquez <cubasve@ECV-MacBook-Pro.local>
2020-05-26 09:55:03 -0400

    Rearrange functions

--- js/main.js
+++ js/main.js
@@ -8,11 +8,11 @@ const categories = [fruitArray, vegArray, snackArray, foodDishArray];
 
 
 /*-------APP'S STATE (VARIABLES)--------*/ 
-let selectedCategory;
-let randomWord;
+// let selectedCategory;
+let randomWord = foodDishArray[Math.floor(Math.random() * foodDishArray.length)].toUpperCase();
 // let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let guessedLetters; //add letters the player already guessed
-let answerWord = ['__', '__', '__', '__', '__', '__', '__'];
+let answerWord;      //['__', '__', '__', '__', '__', '__', '__'];
 let wrongGuesses; //just declare, didn't assign
 //message or error message
 //if you guessed all letters
@@ -50,7 +50,7 @@ document.getElementById('snack').addEventListener('click', selectCategory);
 document.getElementById('dishes').addEventListener('click', selectCategory);
 
 //1 event listener for enter a letter
-document.getElementById('letterButton').addEventListener('click', //enter function);
+guessEl.addEventListener('click', guessLetterEventHandler);
 
 //1 event listener for reset button
 document.getElementById('reset').addEventListener('click', initialize);
@@ -75,9 +75,12 @@ function initialize() {
     //render()
 
     // let randomWord = (categories[i][Math.floor(Math.random() * categories[i].length)].toUpperCase();  
-    selectedCategory(); //choose a category
+    // selectedCategory(); //choose a category
+
+    let randomWord = foodDishArray[Math.floor(Math.random() * foodDishArray.length)].toUpperCase();
     let guessedLetters = []; //empty array - player hasn't started guessing yet
-    letWrongGuesses = 0; //game just started - no mistakes yet
+    let wrongGuesses = 0; //game just started - no mistakes yet
+    let answerWord = [];
     render();
 }
 
@@ -132,29 +135,37 @@ function render() {
     //loop through answer word and change HTML to match the contents of that array (renderAnswerHTML)
     //show image based on how many wrong guesses
     //show list of guessed letters
-
-    guessLetterEventHandler(); //verifies the input first
-    
-    // let answerWord.length = randomWord.length;
-    for (let i = 0; i < answerWord.length; i++) {
-        if (guessEl.value === randomWord[i]) {
-            answerWord.innerHTML = guessLetters.value;
-            guessedLetters.push(guessEl.value)
-        } else {
-
-        }
+    let answerWord = [];
+    for (let i = 0; i < randomWord.length; i++) {
+        answerWord.push('___');
     }
-    renderAnswerHTML();
-    // let answerWord.length = randomWord.length;
-    for (let i =0; i < answerWord.length; i++) {
-        // let letter = document.getElementById('')
-        if (guessLetters[i]) {
-            answerWord.innerHTML = guessLetters.value;
-            guessedLetters.push();//
-        } else {
+    fillInBlanksEl.innerHTML = answerWord.join(' ');
+    guessLetterEventHandler(); //verifies the input first --> it calls renderAnswerHTML();
 
-        }
-    }
+
+
+
+
+    // let answerWord.length = randomWord.length; //Not sure about this line of code
+    // for (let i = 0; i < answerWord.length; i++) {
+    //     if (guessEl.value === randomWord[i]) {
+    //         answerWord.innerHTML = guessLetters.value;
+    //         guessedLetters.push(guessEl.value)
+    //     } else {
+
+    //     }
+    // }
+    // renderAnswerHTML();
+    // // let answerWord.length = randomWord.length;
+    // for (let i =0; i < answerWord.length; i++) {
+    //     // let letter = document.getElementById('')
+    //     if (guessLetters[i]) {
+    //         answerWord.innerHTML = guessLetters.value;
+    //         guessedLetters.push();//
+    //     } else {
+
+    //     }
+    // }
 }
 
 function guessLetterEventHandler() {
@@ -162,21 +173,22 @@ function guessLetterEventHandler() {
     //add letter to guessed letters list
     //(if you use answer word array and letter is correct, adjust array)
     //if letter was not a correct guess, increment wrong guesses
-
+    messageEl.innerHTML = null;
     if (guessEl.length !== 1 || guessEl === null || isNaN(guessEl) === false) {
-        messageEl.innerHTML = 'Please enter a SINGLE LETTER';
-    } else {
+        messageEl.innerHTML = 'Please enter a single letter!';
+    } else if (guessEl.length === 1) {
         guessedLetters.push(guessEl.value);
         if (randomWord[i].includes(guessEl.value)) { //randomWord[i] === guessEl.value OR randomWord[i].includes(guessEl.value)
-            answerWord.push(guessEl.value);
             messageEl.innerHTML = 'Good guess!';
-            fillInBlanksEl.innerHTML = answerWord.join(' ');
+            renderAnswerHTML();
         } else {
-            messageEl = 'Try again!'
+            messageEl.innerHTML = 'Try again!'
             wrongGuesses++;
             changeImages();
         }
     }
+    messageEl.innerHTML = null;
+    guessEl.value = null; //clears input box so user can enter in a new letter
 }
 
 function changeImages() {
@@ -204,10 +216,6 @@ function renderAnswerHTML() {
 if (randomWord[i].includes(guessEl.value)) {
     answerWord.
 }
-
-
-
-
 if (randomWord.includes(guessEl.value)) { //if the guess is in the randomWord
 
 
@@ -223,9 +231,3 @@ function renderAnswerHTML() {
     }
     fillInBlanksEl.innerHTML = answerHTML.join(' ');
 }
-
-
-
-// if (guessedLetter[guessedLetter.length-1].includes(answerWord[i])) {
-//     answerWord.push(guessedLetter[guessedLetters.length-1]);
-//     messageEl.innerHTML = 'Good guess!'
