553b7a11192a1ed088a2d64c8be0f995985f1f68
cubasve <62129720+cubasve@users.noreply.github.com>
2020-05-27 19:50:46 -0400

    Combine win & lose functions into 1

--- js/main.js
+++ js/main.js
@@ -6,21 +6,20 @@ const foodDishArray = ['perogies', 'lasagna', 'pho', 'poutine', 'sushi', 'pizza'
 
 
 /*-------APP'S STATE (VARIABLES)--------*/ 
-
-let randomWord;
-let guessedLetters; //add letters the player already guessed
-// let answerWord;      //['__', '__', '__', '__', '__', '__', '__'];
-let wrongGuesses; 
+//declare the variables, don't assign tem
+let randomWord; //choose a random word from foodDishArray above
+let guessedLetters; //an array --> letters are added when player guesses one
+let wrongGuesses;  //keeps tracks of wrongGuesses of player - max of 6
 
 
 /*-------CACHED ELEMENT REFERENCES--------*/ 
 let guessEl = document.getElementById('letterBox'); //get value of user's input
-let messageEl = document.getElementById('message');
-let fillInBlanksEl = document.getElementById('answer');
-let addLetterEl = document.getElementById('guessedLetters');
+let messageEl = document.getElementById('message'); //tells the player something (enter smth else, you win, you lose, etc.)
+let fillInBlanksEl = document.getElementById('answer'); //The dashes that the user has to guess
+let addLetterEl = document.getElementById('guessedLetters'); //shows letters being added onto guessLetters array in HTML
 
 //references to images
-let emptyHangman = document.querySelector('.one.empty');
+let emptyHangman = document.querySelector('.one.empty'); 
 let headHangman = document.querySelector('.two.head');
 let bodyHangman = document.querySelector('.three.body');
 let armHangman = document.querySelector('.four.arm');
@@ -30,7 +29,7 @@ let fullHangman = document.querySelector('.seven.dead');
 
 /*-------EVENT LISTENERS--------*/ 
 
-//1 event listener to input a letter
+//event listener to input a letter
 document.getElementById('letterButton').addEventListener('click', guessLetterEventHandler);
 
 //1 event listener to restart the game
@@ -50,13 +49,13 @@ function initialize() {
     //already declared in the global scope - don't redeclare them, just assign them
 
     randomWord = foodDishArray[Math.floor(Math.random() * foodDishArray.length)].toUpperCase(); 
-    guessedLetters = []; //don't redeclare --> just assign
+    guessedLetters = []; 
     wrongGuesses = 0; //game just started - no mistakes yet
     // answerWord = [];
-    addLetterEl.innerHTML = null;
-    guessEl.value = null;
-    messageEl.innerHTML = null;
-    emptyHangman.style.display = 'block';
+    addLetterEl.innerHTML = null; //makes guessLetters array in HTML blank when you reset game
+    guessEl.value = null; //makes input value null when restarting
+    messageEl.innerHTML = null; //makes the message null too
+    emptyHangman.style.display = 'block'; //want the emptyHangman to always be on HTML
     render();
     // renderHangman();
 }
@@ -65,41 +64,31 @@ function render() {
     //loop through answer word and change HTML to match the contents of that array (renderAnswerHTML)
     //show image based on how many wrong guesses
     //show list of guessed letters
-    renderAnswerHTML();
-
-    renderHangman();
-    winGame();
-    loseGame();
+    letterButton.disabled = false;
+    renderAnswerHTML(); //initially, this function will present underscores on HTML - later on in game, same function can add letters to underscores
+    renderHangman(); //how should the hangman look
+    winOrLose();
 }
 
-function winGame() {
-    for (let i = 0; i < randomWord.length; i++) { //return: breaks out of function
-        if (!guessedLetters.includes(randomWord[i])) {
-            return false; //haven't won yet --> if you come across a letter that doesn't exist, you return false --> haven't won yet
-        } //if false, it won't run below code
-    }
-    // return true; --> goes through rest of loop if return false
-    messageEl.innerHTML = 'YOU WIN!'; 
-    letterButton.disabled = true; //if all randomWord letters are included in guessedLetters, then you win
-}
-//if it finishes the loop without returning false, we can assume that there are no more spaces left
-//if you go through whole loop and you enver return false, then all the letter were in guessed letters
 
-
-// function winOrLose() { //refactor into 1 function
-//     for (let i = 0; i < randomWord.length; i++) {
-
-// }
-
-
-function loseGame() {
+function winOrLose() { 
     if (wrongGuesses === 6) {
         messageEl.innerHTML = 'YOU LOSE'; 
+        return;
     }
-}
-//if it finishes the loop without returning false, we can assume that there are no more spaces left
-//if you go through whole loop and you enver return false, then all the letter were in guessed letters
 
+    for (let i = 0; i < randomWord.length; i++) {
+        if (!guessedLetters.includes(randomWord[i])) { //return: breaks out of function
+            return false; //haven't won yet --> if you come across a letter that doesn't exist, you return false
+        } //if false, it won't run below code
+        //if it finishes the loop without returning false, we can assume that there are no more spaces left
+        //if you go through whole loop and you never return false, then all the letters were in guessedLetters
+        //goes through rest of loop if return false
+    }
+    messageEl.innerHTML = 'YOU WIN!';
+    letterButton.disabled = true; 
+    return true; //if all randomWord letters are included in guessedLetters, then you win
+}
 
 
 function renderAnswerHTML() {
@@ -120,12 +109,6 @@ function renderAnswerHTML() {
 //if not, an underscore is pushed to answerHTML
 //answerWord = ___ ___ ___ ___ ___ ___
 //answerHTML: changes the answerWord as the user enters a guessedLetters
-
-function loseGame() {
-    if (wrongGuesses === 6) {
-        messageEl.innerHTML = 'YOU LOSE'; 
-    }
-}
 
 
 function renderAnswerHTML() {
